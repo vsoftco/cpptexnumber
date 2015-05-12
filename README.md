1ea7ff22a24192746abd9f5b5f5834375485055a
vsoftco <vsoftco@gmail.com>
2015-05-12 18:41:20 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # cpptexnumber
-C++11 program for renumbering latex references
+## C++11 program for renumbering latex references
+
+### Building without a build system
+  
+Compile with 
+    
+    g++ -std=c++11 -O3 cpptexnumber.cpp -o cpptexnumber
+
+Use the `-DDO_NOT_IGNORE_COMMENTS` flag 
+if you do not want to ignore the comments in the latex source file 
+(ignored by default). A `CMakeLists.txt` is provided for automatic 
+building via [cmake](http://www.cmake.org/) and 
+[g++](https://gcc.gnu.org/)/[clang++](http://clang.llvm.org/). 
+The compiler must be C++11 compliant and must fully support the 
+`std::regex` Regular Expression part of the C++ Standard Library. 
+I recommend `[g++](https://gcc.gnu.org/)` version 4.9
+or later, or [clang++](http://clang.llvm.org/) version 3.5 or later.
+
+### Building with [cmake](http://www.cmake.org/):
+
+I recommend an out-of-source build, i.e., from the root of the project type
+
+    mkdir ./build
+    cd ./build
+    cmake ..
+    make
+
+Use `cmake -DDO_NOT_IGNORE_COMMENTS=ON ..` if you do not want to 
+ignore the comments in the latex source file (ignored by default).
+
+The above commands build the relase version (default) 
+executable `cpptexnumber`, from the source file `./cpptexnumber.cpp`.
+
+### Usage: 
+    
+        cpptexnumber in.tex out.tex pattern replacement 
+
+### Description: replaces all labels in `in.tex` that match 
+a given input pattern, such as `\label{pattern*}` 
+and all corresponding references (`\ref`, `\eqref`, `\pageref`) 
+with renumbered ones `replacement+idx`, where `idx` counts 
+the matching `\label{pattern*}` order of appearance, 
+i.e. equals 1 for the first `\label` that matches the input pattern, 
+2 for the second etc. The `in.tex` is left unchanged and the 
+modifications are written to `out.tex`.
+
+### Example: consider a latex file `source.tex` having three labels, named, 
+in order of appearance, `\label{eqn5}`, `\label{eqn_important}` 
+and lastly `\label{entropy}`. Then the command 
+
+        cpptexnumber in.tex out.tex eqn Eqn 
+
+will replace `\label{eqn5}` by `\label{Eqn1}` (and also all references 
+to `eqn5` will automatically become references to `Eqn1`) and replace 
+`\label{eqn_important}` by `\label{Eqn2}` (again together with 
+the corresponding references). The `\label{entropy}` remains unchanged 
+since it does not match the input pattern `\label{eqn*}`. 
+The changes are saved to the file `out.tex`.
---
+++ cpptexnumber.cpp
@@ -0,0 +1,206 @@
+// Replaces latex labels and references
+// Compile with g++ -std=c++11 -O3 cpptexnumber.cpp -o cpptexnumber
+// Use the -DDO_NOT_IGNORE_COMMENTS flag 
+// if you do not want to ignore the comments (ignored by default)
+
+#include <cstddef>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#define DEBUG_PRINT(x) std::cout << (x) << std::endl
+
+using label_idx_map = std::map<std::string, std::size_t>;
+using idx_label_map = std::map<std::size_t, std::string>;
+
+// build a map sorted by value from a standard map sorted by key
+idx_label_map by_value(const label_idx_map& labels)
+{
+    idx_label_map result;
+    for (auto && elem : labels)
+        result[elem.second] = elem.first;
+    return result;
+}
+
+label_idx_map build_labels(std::ifstream& ifile,
+                           const std::string& pattern_in)
+{
+    label_idx_map result;
+    std::regex re{R"(\\label\{)" + pattern_in + R"(.*?\})"};
+    std::smatch labels;
+    std::string line;
+    std::size_t line_no = 0, label_no = 1;
+    while (std::getline(ifile, line))
+    {
+        ++line_no;
+#ifndef DO_NOT_IGNORE_COMMENTS
+        std::getline(std::istringstream {line}, line, '%');
+#else
+        std::getline(std::istringstream {line}, line);
+#endif
+        // search regex in current line
+        while (std::regex_search (line, labels, re))
+        {
+            for (auto && label : labels) // matches
+            {
+                // strip "content" from \label{content}
+                std::string label_content =
+                    label.str().substr(7, label.str().size() - 8);
+                if (result.find(label_content) != result.end())
+                {
+                    std::cout << "PARSING ERROR: Duplicate \\label{"
+                              << label_content << "} on line "
+                              << label_no << std::endl;
+                    std::exit(EXIT_FAILURE);
+                }
+                result[label_content] = label_no++;
+            }
+            // continue with the remainder of the line
+            line = labels.suffix().str();
+        }
+    }
+    return result;
+}
+
+std::string replace_line(std::string line,
+                         const std::string& pattern_in,
+                         const std::string& pattern_out,
+                         const label_idx_map& labels,
+                         const std::vector<std::string>& refs,
+                         std::size_t line_no)
+{
+    for (auto & elem : refs) // for all reference types
+    {
+        std::string::size_type start;
+        std::string::size_type end = 0;
+        while (true)
+        {
+            start = line.find(elem, end);
+            if (start == std::string::npos)
+                break;
+            end = line.find("}", start);
+            if (end == std::string::npos)
+            {
+                std::cout << "PARSING ERROR: No matching '}'"
+                          << " on line " << line_no << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
+
+            auto start_replace = start + elem.size();
+            auto count = end - (start + elem.size());
+            std::string ref = line.substr(start_replace, count);
+            auto found = labels.find(ref);
+            if (found != labels.end())
+            {
+                // construct the new reference
+                std::string new_ref = pattern_out +
+                                      std::to_string(labels.at(ref));
+                line.replace(start_replace, count, new_ref);
+            }
+            // the reference starts with pattern_in
+            // but it is not in the labels map
+            else if (ref.compare(0, pattern_in.size(), pattern_in) == 0)
+            {
+                std::cout << "PARSING WARNING: Undefined reference "
+                          << elem << ref << "} on line " << line_no
+                          << std::endl;
+            }
+        }
+    }
+    return line;
+}
+
+void replace_refs(std::ifstream& ifile,
+                  std::ofstream& ofile,
+                  const std::string& pattern_in,
+                  const std::string& pattern_out,
+                  const label_idx_map& labels,
+                  const std::vector<std::string>& refs)
+{
+    std::string line, line_no_comments;
+    std::size_t line_no = 0;
+
+    while (std::getline(ifile, line))
+    {
+        ++line_no;
+        auto ss = std::istringstream{line};
+        // get the line without comments
+#ifndef DO_NOT_IGNORE_COMMENTS
+        std::getline(ss, line_no_comments, '%');
+#else
+        std::getline(ss, line_no_comments);
+#endif
+        if (line == line_no_comments) // no comments
+        {
+            line = {};
+        }
+        else // we have a comment
+        {
+            std::getline(ss, line);
+            line = '%' + line;
+        }
+        line_no_comments = replace_line(line_no_comments, pattern_in,
+                                        pattern_out, labels, refs, line_no);
+        ofile << line_no_comments + line + '\n'; // write it to output
+
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 5)
+    {
+        std::cout << "Usage: " << argv[0]
+                  << " <in.tex> <out.tex> <pattern> <replacement>"
+                  << std::endl;
+        std::cout << "(c) Vlad Gheorghiu 2015, vsoftco@gmail.com"
+                  << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    std::ifstream ifile(argv[1]);
+    if (!ifile)
+    {
+        std::cout << "SYSTEM ERROR: Can not open the input file "
+                  << argv[1] << std::endl;
+        std::exit(EXIT_FAILURE);
+
+    }
+    std::ofstream ofile(argv[2]);
+    if (!ofile)
+    {
+        std::cout << "SYSTEM ERROR: Can not open the output file "
+                  << argv[2] << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    std::string pattern_in = argv[3];
+    std::string pattern_out = argv[4];
+
+    std::vector<std::string> refs = {"\\label{",
+                                     "\\eqref{",
+                                     "\\ref{",
+                                     "\\pageref{"
+                                    };
+    auto labels = build_labels(ifile, pattern_in);
+    if (labels.size() == 0)
+    {
+        std::cout << "PARSING ERROR: pattern <" << pattern_in
+                  << "> not found" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    ifile.clear();
+    ifile.seekg(0);
+    replace_refs(ifile, ofile, pattern_in, pattern_out, labels, refs);
+
+    std::cout << "REPLACED: " << std::endl;
+    for (auto && elem : by_value(labels))
+    {
+        std::cout << elem.second << " -> " <<
+                  (pattern_out + std::to_string(elem.first)) << std::endl;
+    }
+}
